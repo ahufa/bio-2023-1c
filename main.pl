4bf756904bbbb362d0ba75469c89e33eb52e34ee
facundoahumada <facundo.ahumada@despegar.com>
2023-06-05 20:01:50 -0300

    Segunda iteracion

--- main.pl
+++ main.pl
@@ -3,7 +3,6 @@
 #use Bio::Perl;
 use Bio::SeqIO;
 
-
 if (scalar(@ARGV) == 0) {
     print "No se pasaron argumentos al programa!\n";
     exit
@@ -16,11 +15,21 @@ if (!($file =~ /\.gb$/)){
 	exit;
 }
 
-my $input = Bio::SeqIO->new(-file => $file, -format => 'genbank');
-my $output = Bio::SeqIO->new(-file => ">result.fsa", -format => 'fasta');
-
-while (my $seq = $input->next_seq) {
-    $output->write_seq($seq);
-}
+to_fasta($file, 1);
 
 print "Transcripcion finalizada!\n";
+
+sub to_fasta{
+    my ($origin) = $_[0];
+    my ($frame) = $_[1];
+
+    my $result_file_name = substr($origin, 0, -3)."_frame_".$frame.".fsa";
+
+    my $input = Bio::SeqIO->new(-file => $origin, -format => 'genbank');
+    my $output = Bio::SeqIO->new(-file => ">$result_file_name", -format => 'fasta');
+
+    while (my $seq = $input->next_seq) {
+        my $translated = $seq->translate(-frame => $frame);
+        $output->write_seq($translated);
+    }
+}
