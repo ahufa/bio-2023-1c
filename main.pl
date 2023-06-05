e9d37b9122ff1d3691e1f9a771a7de1a2009f021
facundoahumada <facundo.ahumada@despegar.com>
2023-06-05 19:07:36 -0300

    Primer intento de funcion

---
+++ main.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w 
+
+use Bio::Perl;
+
+if (scalar @ARGV = 0) {
+    print "No se pasaron argumentos al programa!\n";
+    exit
+} 
+
+my $file = @ARGV[0];
+
+if (!($file =~ /\.gb$/)){
+    print "El archivo $file no cumple con el tipo requerido!\n";
+	exit;
+}
+
+my $input = Bio::SeqIO->new(-file => $file, -format => 'genbank');
+my $output = Bio::SeqIO->new(-file => ">result.fsa", -format => 'fasta');
+
+while (my $seq = $input->next_seq) {
+    $output->write_seq($seq);
+}
+
+print "Transcripcion finalizada!\n";
