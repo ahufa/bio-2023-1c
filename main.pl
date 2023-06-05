2ed4dfaa88ef9ae8ff9afd4675bfafb770809167
Cristian Egüez <cristian.eguez@despegar.com>
2023-06-05 19:26:01 -0300

    1 - basiso, funciona

--- main.pl
+++ main.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/perl -w 
 
-use Bio::Perl;
+#use Bio::Perl;
+use Bio::SeqIO;
 
-if (scalar @ARGV = 0) {
+
+if (scalar(@ARGV) == 0) {
     print "No se pasaron argumentos al programa!\n";
     exit
 } 
 
-my $file = @ARGV[0];
+my $file = $ARGV[0];
 
 if (!($file =~ /\.gb$/)){
     print "El archivo $file no cumple con el tipo requerido!\n";
